770fec5b5391f036a8112a7022ce61e22b4aa30c
ronaks9065 <115705180+ronaks9065@users.noreply.github.com>
2025-03-13 12:58:34 +0530

    Update owasp_dependency.groovy

--- vars/owasp_dependency.groovy
+++ vars/owasp_dependency.groovy
@@ -1,4 +1,10 @@
-def call(){
-  dependencyCheck additionalArguments: '--scan ./', odcInstallation: 'OWASP'
+// def call(){
+//   dependencyCheck additionalArguments: '--scan ./', odcInstallation: 'OWASP'
+//   dependencyCheckPublisher pattern: '**/dependency-check-report.xml'
+// }
+
+def call() {
+  dependencyCheck additionalArguments: "--scan ./ --nvdApiKey ${env.NVD_API_KEY}", odcInstallation: 'OWASP'
   dependencyCheckPublisher pattern: '**/dependency-check-report.xml'
 }
+
