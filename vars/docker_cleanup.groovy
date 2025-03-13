c643193a13ed591bf35497e1f3f5a44beb9189ac
ronaks9065 <ronak.prajapati@caxsol.com>
2025-03-13 09:46:28 +0530

    Update docker cleanup function

--- vars/docker_cleanup.groovy
+++ vars/docker_cleanup.groovy
@@ -1,8 +1,3 @@
-// def call(String Project, String ImageTag, String DockerHubUser){
-//   sh "docker rmi ${DockerHubUser}/${Project}:${ImageTag}"
-// }
-def docker_cleanup() {
-    sh """
-        docker image prune -a -f
-    """
+def call(String Project, String ImageTag, String DockerHubUser){
+  sh "docker rmi ${DockerHubUser}/${Project}:${ImageTag}"
 }
