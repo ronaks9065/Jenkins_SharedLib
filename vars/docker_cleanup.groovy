c71c9281a5816eb40b7b683e2c25262be2a3da42
ronaks9065 <115705180+ronaks9065@users.noreply.github.com>
2025-03-13 19:09:03 +0530

    Update docker_cleanup.groovy

--- vars/docker_cleanup.groovy
+++ vars/docker_cleanup.groovy
@@ -1,3 +1,4 @@
 def call(String Project, String ImageTag, String DockerHubUser){
   sh "docker rmi ${DockerHubUser}/${Project}:${ImageTag}"
+  sh "docker rmi -f \$(docker images -f \"dangling=true\" -q)"
 }
